e1cd766ca2d17230142c6a6600504871f5014543
AkshatJoshi-18 <akshatjoshi8630@google.com>
2026-01-23 23:59:02 +0530

    feat: creating network and a ec2 as a base of infa

--- terraform/environments/stage/provider.tf
+++ terraform/environments/stage/provider.tf
@@ -8,6 +8,16 @@ terraform {
       version = "6.27.0" # CRITICAL: Version Pinning
     }
   }
+  # REMOTE BACKEND: Essential for team collaboration and locking
+  # Ensure the S3 bucket and DynamoDB table exist before running init
+  backend "s3" {
+    bucket         = "tf-bucket-8630"
+    key            = "compute/ec2/terraform.tfstate"
+    region         = "ap-south-1"
+    # dynamodb_table = "terraform-locks"
+    encrypt        = true
+    use_lockfile = true
+  }
 }
 
 # 2. The "Configuration" Block
@@ -20,6 +30,7 @@ provider "aws" {
   # Best Practice: Auto-tag every resource created by this provider
   default_tags {
     tags = {
+      Owner       = "DevOps-Team"
       Environment = "stage"
       ManagedBy   = "Terraform"
       Project     = "snapcart-infrastructure-and-deployment"
---
+++ terraform/modules/compute/ec2/main.tf
@@ -0,0 +1,57 @@
+resource "aws_security_group" "this" {
+  name        = "${var.project_name}-${var.environment}-sg"
+  description = "Security group for ${var.project_name} ${var.environment} environment"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_cidr_blocks
+    description = "Allow SSH access"
+  }
+
+  ingress {
+    from_port   = 443   
+    to_port     = 443   
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_cidr_blocks
+    description = "Allow HTTPS access"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-sg"
+    Project     = var.project_name
+    Environment = var.environment
+  }
+}
+
+resource "aws_instance" "this" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+    subnet_id = var.subnet_id
+    vpc_security_group_ids = [aws_security_group.this.id]
+    
+    root_block_device {
+        volume_size = 30
+        volume_type = "gp3"
+        encrypted = true
+    }
+
+    lifecycle {
+        create_before_destroy = true
+    }
+    
+    tags = {
+        Name = "${var.project_name}-${var.environment}-instance"
+        Project = var.project_name
+        Environment = var.environment
+    }
+}
